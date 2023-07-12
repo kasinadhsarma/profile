7a5d156056e536a187f99d3ed18fceb9278e17c0
swayampakula.v.s.s.pavanakasinadha sarma <81065703+kasinadhsarma@users.noreply.github.com>
2023-07-12 18:39:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
-for (let i = 0; i < 200; i++) { // 200 stars
+for (let i = 0; i < 200; i++) {
     let star = document.createElement('div');
     star.className = 'star';
     star.style.top = Math.random() * window.innerHeight + 'px';
     star.style.left = Math.random() * window.innerWidth + 'px';
     document.body.appendChild(star);
 }
+
+let rockets = document.querySelectorAll('.rocket');
+rockets.forEach((rocket, index) => {
+    if (index % 2 === 0) {
+        rocket.style.left = Math.random() * window.innerWidth + 'px';
+    } else {
+        rocket.style.top = Math.random() * window.innerHeight + 'px';
+    }
+});
